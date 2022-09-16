00ecf8a9af870ddd108cf002047290df9d990386
Julian <julianogrady07@gmail.com>
2022-09-15 22:23:24 -0400

    ContextButton Broke

---
+++ src/components/ContextButton.vue
@@ -0,0 +1,27 @@
+<template>
+	<button class="context-button">
+		<img class="context-button-image" src="src/assets/menu_lines.svg">
+	</button>
+</template>
+
+<style>
+	.context-button{
+		opacity: 0%;
+		transition-duration: 0.2s;
+
+		width: 40px;
+		height: 40px;
+
+		margin-right: 10px;
+
+		background-color: rgba(0,0,0,0);
+		outline: none;
+	}
+	
+	.context-button-image{
+		width: 100%;
+		height: 100%;
+
+		border: 0;
+	}
+</style>
